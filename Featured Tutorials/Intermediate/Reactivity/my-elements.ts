7d445fc0205faae70787dc8d10358da2c390300f
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-08 23:53:13 -0600

    paso5

--- Featured Tutorials/Intermediate/Reactivity/my-elements.ts
+++ Featured Tutorials/Intermediate/Reactivity/my-elements.ts
@@ -29,23 +29,39 @@ export class MyElement extends LitElement {
   shouldUpdate(changedProperties: PropertyValues<this>): boolean {
     return !(changedProperties.size === 1 && changedProperties.has('duration'));
   }
-
-  render() {
-    this._renderCount++;
-    const keyframeOptions = { duration: this.duration };
-
-    return html`
-      <p>
-        <button @click=${() => (this.big = !this.big)}>Animate</button>
-      </p>
-      <p>
-        <label>Speed <select @change=${this.setDuration}>
-          <option value="250" selected>Fast</option>
-          <option value="1500">Slow</option>
-        </select></label>
-        Render count: ${this._renderCount}
-      </p>
-      <p class="bar ${classMap({big: this.big})}" ${animate({keyframeOptions})}></p>
-    `;
+  import {LitElement, html, PropertyValues} from 'lit';
+  import {customElement, property} from 'lit/decorators.js';
+  
+  @customElement('my-element')
+  export class MyElement extends LitElement {
+    @property() forward = '';
+    @property() backward = '';
+  
+    willUpdate(changedProperties: PropertyValues<this>) {
+      if (changedProperties.has('forward')) {
+        this.backward = this.forward.split('').reverse().join('');
+      }
+  
+      if (changedProperties.has('backward')) {
+        this.forward = this.backward.split('').reverse().join('');
+      }
+    }
+  
+    onInput(e: Event) {
+      const inputEl = e.target as HTMLInputElement;
+      if (inputEl.id === 'forward') {
+        this.forward = inputEl.value;
+      } else {
+        this.backward = inputEl.value;
+      }
+    }
+  
+    render() {
+      return html`
+        <label>Forward: <input id="forward" @input=${this.onInput} .value=${this.forward}></label>
+        <label>Backward: <input id="backward" @input=${this.onInput} .value=${this.backward}></label>
+        <div>Forward text: ${this.forward}</div>
+        <div>Backward text: ${this.backward}</div>
+      `;
+    }
   }
-}
