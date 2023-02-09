f3227b15ebd201457b71165ba421d7acac958e00
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-09 00:51:48 -0600

    Paso7

--- Featured Tutorials/Advanced/Intro to async directives/time-ago.ts
+++ Featured Tutorials/Advanced/Intro to async directives/time-ago.ts
@@ -1,16 +1,18 @@
 import {format} from 'timeago.js';
 import {directive, AsyncDirective} from 'lit/async-directive.js';
-import {Part, DirectiveParameters} from 'lit/directive.js';
+import type {Part, DirectiveParameters} from 'lit/directive.js';
 
 class TimeAgoDirective extends AsyncDirective {
 
   timer: number | undefined;
+  time!: Date;
 
   render(time: Date) {
     return format(time);
   }
 
   update(part: Part, [time]: DirectiveParameters<this>) {
+    this.time = time;
     if (this.isConnected) {
       this.ensureTimerStarted();
     }
@@ -20,7 +22,7 @@ class TimeAgoDirective extends AsyncDirective {
   ensureTimerStarted() {
     if (this.timer === undefined) {
       this.timer = setInterval(() => {
-        /* do some periodic work */
+        this.setValue(this.render(this.time));
       }, 1000);
     }
   }
