46537e548f2472a3532f7357d2de7a17ddeff163
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-02-08 22:17:24 -0600

    Paso7

--- Featured Tutorials/Beginner/Working with lists/my-elements.ts
+++ Featured Tutorials/Beginner/Working with lists/my-elements.ts
@@ -1,38 +1,31 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {repeat} from 'lit/directives/repeat.js';
+import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  tasks = [
-    { id: 'a', label: 'Learn Lit'},
-    { id: 'b', label: 'Feed the cat'},
-    { id: 'c', label: 'Go for a walk'},
-    { id: 'd', label: 'Take a nap'},
+  things = [
+    "Raindrops on roses",
+    "Whiskers on kittens",
+    "Bright copper kettles",
+    "Warm woolen mittens",
   ];
 
   render() {
     return html`
-      <p>Things to do today:</p>
-      <button @click=${() => this._sort(1)}>Sort ascending</button>
-      <button @click=${() => this._sort(-1)}>Sort descending</button>
+      <p>A few of my favorite things</p>
       <ul>
-        ${repeat(
-          this.tasks,
-          (task) => task.id,
-          (task) => html`
+        <!-- TODO: Add click event handlers for the delete button  below. -->
+        ${map(
+          this.things,
+          (thing, index) => html`
             <li>
-              <label><input type="checkbox" />${task.id}) ${task.label}</label>
+              ${thing}
+              <button>Delete</button>
             </li>
           `
         )}
       </ul>
     `;
   }
-
-  private _sort(dir: number) {
-    this.tasks.sort((a, b) => a.label.localeCompare(b.label) * dir);
-    this.requestUpdate();
-  }
-}
